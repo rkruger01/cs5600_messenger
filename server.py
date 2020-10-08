a06a4ba3344af131651a658998b2a32619224402
Ryan Kruger <ryan.kruger91@gmail.com>
2020-10-08 14:51:48 -0500

    Server accepts multiple connections
    Currently, the server is set to spawn a thread for each new process. It currently doesn't kill these processes, and the client doesn't have the ability to send and receive freely, as it always waits for a new connection.

--- server.py
+++ server.py
@@ -1,19 +1,43 @@
 import socket
+import threading
 
+# loopback only
+# predefined port
+# predefined max connections
 HOST = '127.0.0.1'
 PORT = 4252
+MAX_CONNECTIONS = 5
+
+
+def client_mgr(conn, addr):
+    while True:
+        message = conn.recv(2048)
+        if message:
+            # handle client message here
+            # if control message, perform function on the server
+            # if non-control message, broadcast message
+            msg_handler(conn, addr, message)
+        else:
+            # remove client from the list of connected clients
+            pass
+
+
+def msg_handler(conn, addr, message):
+    # assume message is not control message
+    for t in active_connections:
+        if t is not conn:
+            # found non-self target for message
+            t.send(message)
+    pass
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
-    s.listen(5)
-    conn, addr = s.accept()
-    with conn:
-        print("Connected to", addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print(addr, " Terminated")
-                break
-            msg = data.decode()
-            newMsg = addr[0] + ":" + msg
-            conn.sendall(newMsg.encode())
+    s.listen(MAX_CONNECTIONS)
+    active_connections = []
+    while True:
+        conn, addr = s.accept()
+        newThread = threading.Thread(target=client_mgr, args=(conn, addr), name=addr)
+        active_connections.append(conn)
+        newThread.start()
