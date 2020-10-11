480cb873cbc071580f2fe2d0eb220ed219c587bd
Ryan Kruger <ryan.kruger91@gmail.com>
2020-10-10 20:07:11 -0500

    Implemented exception catching in client and server. Client can now request to disconnect from server safely.
    Implemented /quit functionality to client and server. If the server recieves a message consisting of /quit, it will disconnect the client from the server and close the socket, and the client will disconnect as well.
    
    Additionally, error checking was added to the server. Now, if a connection is terminated from the server in an unexpected way, the server removes the connection from it's internal list of active connections and closes the socket. Similarly, error checking was implemented in the client. Although the connection still closes, it prevents display of any error messages to the user.

--- client.py
+++ client.py
@@ -8,16 +8,31 @@ PORT = 4252
 
 def send_handler(s):
     while True:
-        s.send(input().encode())
+        msg = input()
+        if msg == "/quit":
+            s.send("/quit".encode())
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+            break
+        s.send(msg.encode())
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    sender = threading.Thread(target=send_handler,args=(s,))
+    sender = threading.Thread(target=send_handler, args=(s,))
     sender.start()
     while True:
+        if s.fileno() == -1:
+            # socket closed
+            break
         r, _, _ = select.select([s], [], [])
         for rs in r:
             if s == rs:
-                data = rs.recv(1024)
+                try:
+                    data = rs.recv(1024)
+                except OSError:
+                    # connection terminated (for some reason)
+                    break
+                if not data:
+                    break
                 print(data.decode())
--- server.py
+++ server.py
@@ -11,13 +11,27 @@ MAX_CONNECTIONS = 5
 
 def client_mgr(conn, addr):
     while True:
-        message = conn.recv(1024)
+        try:
+            message = conn.recv(1024)
+        except ConnectionResetError:
+            # Connection failed, possibly due to a non-expected termination on client side
+            # i.e. client crashed or force closed
+            active_connections.remove(conn)
+            conn.shutdown(socket.SHUT_RDWR)
+            conn.close()
+            break
         if message:
             # handle client message here
-            # if control message, perform function on the server
-            control_msg_handler(conn, addr, message)
+            message = message.decode()
+            print(addr, ":", message)
+            if message[0] == "/":
+                print("Control message: ", message)
+                # if control message, perform function on the server
+                if not control_msg_handler(conn, message):
+                    # control_msg_handler returns False, terminating connection
+                    break
             # if non-control message, broadcast message
-            msg_handler(conn, addr, message)
+            msg_handler(conn, message)
         else:
             # message is empty. Do we kill the connection, or do we send an error message?
             # prevent empty message sent from client side?
@@ -25,18 +39,31 @@ def client_mgr(conn, addr):
             pass
 
 
-def control_msg_handler(conn, addr, message):
+def control_msg_handler(conn, message):
+    if message == "/quit":
+        print(conn, " disconnecting")
+        if conn in active_connections:
+            active_connections.remove(conn)
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
+        return False
     # message is special command
     # i.e. /nickname, /msg (private message), /exit or /quit, etc.
     pass
 
 
-def msg_handler(conn, addr, message):
+def msg_handler(conn, message):
     # message is not control message
     for t in active_connections:
         if t is not conn:
             # found non-self target for message
-            t.send(message)
+            try:
+                t.send(message.encode())
+            except ConnectionAbortedError:
+                # client no longer exists, remove from valid sender list
+                active_connections.remove(t)
+                t.shutdown(socket.SHUT_RDWR)
+                t.close()
     pass
 
 
