f4b01b25d0e42bd4409d2d8b6eb29b9795c250df
Ryan Kruger <ryan.kruger91@gmail.com>
2020-11-15 17:49:29 -0600

    Message functionality now uses pickle module. Implemented color changes. Updated internal documentation.
    Message objects now use the python Pickle module to convert lists to bytes that are sent over the socket connection. This allows for modular messages, which will make further development of features much easier.
    
    Users now have a default color associated with their messages (#000000), and are allowed to change it to a valid hexidecimal color code in a message to the server.
    
    Internal documentation of server and client functions were updated. Documentation of message objects was added to client.py for easy reference.
    
    TODO: Move documentation, further extend server messaging functionality.

--- client.py
+++ client.py
@@ -1,6 +1,29 @@
 import socket
 import threading
 import select
+import pickle
+'''
+MESSAGE OBJECTS
+
+A message object going from the client to the server is a 2-list consisting of 
+
+[isSystemMessage, message]
+
+where
+
+isSystemMessage is a Bool that describes whether the server should treat the received message as a control message,
+message is a String that contains the message.
+
+A message object going from the server to the client is a 4-list consisting of 
+
+[isSystemMessage, colorCode, messageSender, message]
+
+where
+isSystemMessage is a Bool that describes whether the client should treat the received message as a control message,
+colorCode is a String containing the hex color code of the message (#FFFFFF if the message is a system message),
+messageSender is a String containing the source of the message (SERVER if the message is a system message),
+message is a String that contains the message.
+'''
 
 HOST = '127.0.0.1'
 PORT = 4252
@@ -9,12 +32,20 @@ PORT = 4252
 def send_handler(s):
     while True:
         msg = input()
-        if msg == "/quit":
-            s.send("/quit".encode())
-            s.shutdown(socket.SHUT_RDWR)
-            s.close()
-            break
-        s.send(msg.encode())
+        if msg.startswith("/"):
+            if msg == "/quit":
+                msgList = [True, "/quit"]
+                msg = pickle.dumps(msgList)
+                s.send(msg)
+                s.shutdown(socket.SHUT_RDWR)
+                s.close()
+                break
+            #sends non-quit command message, continues execution
+            msg = pickle.dumps([True, msg])
+            s.send(msg)
+            continue
+        msg = pickle.dumps([False, msg])
+        s.send(msg)
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -35,4 +66,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     break
                 if not data:
                     break
-                print(data.decode())
+                msg = pickle.loads(data)
+                print(msg)
--- server.py
+++ server.py
@@ -1,7 +1,18 @@
 import socket
+from string import hexdigits
 import threading
 import hashlib
 import re
+import pickle
+
+# Dictionary of alert messages
+# Allows for unified updates across codebase, easier localization
+serverAlertMessages = {
+    "NICKUPDATE": "Nickname updated to ",
+    "NICKBADARGS": "Expected usage: /nick [new name]",
+    "COLORUPDATE": "Color updated to ",
+    "COLORBADARGS": "Expected usage: /color hexcolor"
+}
 
 # loopback only
 # predefined port
@@ -15,8 +26,10 @@ class User:
     nick = ""
     conn = None
     addr = ""
+    color = ""
 
-    def __init__(self, conn, addr, nick):
+    def __init__(self, conn, addr, nick, color="#000000"):
+        self.color = color
         self.nick = nick
         self.conn = conn
         self.addr = addr
@@ -44,7 +57,8 @@ def cfg_file_generator():
         passwd = input()
         if passwd:
             # WARNING: This is NOT a secure way to store a password! This is an unsalted hash, and it is relatively
-            # easy to crack insecure passwords, given the hash. Be careful!
+            # easy to crack insecure passwords, given the hash. For more secure applications, consider salting the
+            # hashes, or not including the password (or the hash) in plaintext at all.
             hashp = hashlib.sha256(passwd.encode()).hexdigest()
             target.write("password=" + hashp + "\n")
         else:
@@ -52,6 +66,9 @@ def cfg_file_generator():
     pass
 
 
+# This handles each client connected to the server. If a client termimates their connection irregularly, i.e. force
+# closes the connection instead of notifying the server and gracefully closing, those errors are caught in here and
+# handled. Afterwards, the thread self-terminates.
 def client_mgr(cli):
     while True:
         try:
@@ -65,17 +82,18 @@ def client_mgr(cli):
             break
         if message:
             # handle client message here
-            message = message.decode()
-            print(cli.addr, ":", message)
-            if message[0] == "/":
-                print("Control message: ", message)
-                # if control message, perform function on the server
-                if not control_msg_handler(cli, message):
-                    # control_msg_handler returns False, terminating connection
+            # decodes message object
+            message = pickle.loads(message)
+            print(cli.addr, ":", message[1])
+            if message[0]:
+                # if message[0] is true for messages sent to the server, that message is a control message
+                # think /quit, /nick, etc.
+                if not control_msg_handler(cli, message[1]):
+                    # control_msg_handler returns False, so we are terminating connection
                     break
             # if non-control message, broadcast message
             else:
-                msg_handler(cli, message)
+                msg_handler(cli, message[1])
         else:
             # message is empty. Do we kill the connection, or do we send an error message?
             # prevent empty message sent from client side?
@@ -83,9 +101,10 @@ def client_mgr(cli):
             pass
 
 
+# Expects: sender to be an instance of class User, message to be a String or similar
+# Handles any control messages sent to the server, for example, nickname changes, color changes, quitting
+# Returns True if client remains connected, False if client has terminated their connection.
 def control_msg_handler(sender, message):
-    # message is special command or emulates special command. "/" is first char
-    # i.e. /nickname, /msg (private message), /exit or /quit, etc.
     if message == "/quit":
         print(sender.nick, " disconnecting")
         if conn in active_connections:
@@ -93,25 +112,49 @@ def control_msg_handler(sender, message):
         sender.conn.shutdown(socket.SHUT_RDWR)
         sender.conn.close()
         return False
+    # splits message apart to handle command arguments
     msg = message.split()
     if msg[0] == "/nick":
-        if msg[1]:
+        # User wants to change nickname\
+        try:
+            # TODO: Validate name (no admin strings, no special characters, no repeats)
             sender.nick = msg[1]
-            sysnotify = "SYSTEM:Nickname updated to " + sender.nick
-        else:
-            sysnotify = "SYSTEM:Expected usage: /nick [new name]"
-        sender.conn.send(sysnotify.encode())
-
+            sysmsg = serverAlertMessages["NICKUPDATE"] + sender.nick
+        except IndexError:
+            sysmsg = serverAlertMessages["NICKBADARGS"]
+        msg = pickle.dumps([True, "#FFFFFF", "SYSTEM", sysmsg])
+        sender.conn.send(msg)
+    # user wants to change their associated message color:
+    if msg[0] == "/color":
+        try:
+            if msg[1].startswith("#"):
+                hexcode = msg[1][1:]
+            else:
+                hexcode = msg[1]
+            if all(c in hexdigits for c in hexcode) and len(hexcode) == 6:
+                sender.color = "#" + hexcode
+                sysmsg = serverAlertMessages["COLORUPDATE"] + sender.color
+            else:
+                sysmsg = serverAlertMessages["COLORBADARGS"]
+        except IndexError:
+            sysmsg = serverAlertMessages["COLORBADARGS"]
+        msg = pickle.dumps([True, "#FFFFFF", "SYSTEM", sysmsg])
+        sender.conn.send(msg)
     return True
 
 
+# Expects: sender to be an instance of class User, message to be a String or similar
+# Function sends message to all connected clients, user included.
+# If a connection no longer exists, msg_handler automatically removes it from the active connection list.
+# Returns: None
 def msg_handler(sender, message):
     # message is not control message
     for t in active_connections:
         # sends message to all users, including sender
         try:
-            formattedMsg = sender.nick + ":" + message
-            t.conn.send(formattedMsg.encode())
+            formattedMsg = [False, sender.color, sender.nick, message]
+            formattedMsg = pickle.dumps(formattedMsg)
+            t.conn.send(formattedMsg)
         except ConnectionAbortedError:
             # client no longer exists, remove from valid sender list
             active_connections.remove(t)
@@ -120,6 +163,7 @@ def msg_handler(sender, message):
     pass
 
 
+# Main
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
@@ -127,6 +171,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     active_connections = []
     while True:
         conn, addr = s.accept()
+        # TODO: Encryption handshake, verify that messages are encrypted
         newActiveUser = User(conn, addr, str(addr))
         newThread = threading.Thread(target=client_mgr, args=(newActiveUser,), name=addr)
         active_connections.append(newActiveUser)
