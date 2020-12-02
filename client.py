f469431a653419162202b72bbbded616f7118195
Ryan Kruger <ryan.kruger91@gmail.com>
2020-12-02 14:11:37 -0600

    Implemented GUI features in client.py from demo.py. Refactored code to work with event-based structure of pycharm.
    Pulled gui code from commit 7a868e35, modified slightly to fit with coding conventions. Client now operates with a graphical frontend instead of command-line based.
    
    Refactored listening code for client.py. The socket listener is now treated as a thread, while the sending thread has been converted to a function that is called by the frontend when a message needs to be sent.
    
    TODO: Implement password authentication, choose from multiple config files,

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import pickle
 import select
 import socket
 import threading
+import tkinter
 
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Cipher.PKCS1_OAEP import PKCS1OAEP_Cipher
@@ -33,24 +34,24 @@ message is a String that contains the message.
 '''
 
 
-def send_handler(s, serverEncryptor: PKCS1OAEP_Cipher):
-    while True:
-        msg = input()
-        if msg.startswith("/"):
-            if msg == "/quit":
-                msgList = [True, "/quit"]
-                msg = pickle.dumps(msgList)
-                msg = serverEncryptor.encrypt(msg)
-                s.send(msg)
-                s.shutdown(socket.SHUT_RDWR)
-                s.close()
-                break
-            # sends non-quit command message, continues execution
-            msg = pickle.dumps([True, msg])
-        else:
-            msg = pickle.dumps([False, msg])
-        msg = serverEncryptor.encrypt(msg)
-        s.send(msg)
+def send_handler(s, serverEncryptor: PKCS1OAEP_Cipher, entryObject: tkinter.Entry):
+    msg = entryObject.get()
+    entryObject.delete(0, 'end')
+    print("sending ", msg)
+    if msg.startswith("/"):
+        if msg == "/quit":
+            msgList = [True, "/quit"]
+            msg = pickle.dumps(msgList)
+            msg = serverEncryptor.encrypt(msg)
+            s.send(msg)
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+        # sends non-quit command message, continues execution
+        msg = pickle.dumps([True, msg])
+    else:
+        msg = pickle.dumps([False, msg])
+    msg = serverEncryptor.encrypt(msg)
+    s.send(msg)
 
 
 def keyExchange(s, clientRSAKeypair, clientEncryptor):
@@ -69,10 +70,32 @@ def serverConfigParser():
     configFileList = [x for x in os.listdir('.') if os.path.isfile(os.path.join('.', x)) and x.endswith('.echat')]
     for f in configFileList:
         # If there are multiple configuration files, choose the correct one here
+        # TODO: Allow users to select correct configuration file
         config.read(f)
         print(config['SERVER']['serverNICKNAME'])
     return config['SERVER']['ServerIP'], config['SERVER']['ServerPORT'], config['SERVER']['ServerPASSWORD'], \
            config['SERVER']['ServerNICKNAME']
+
+
+def listener(msgList: tkinter.Listbox, s: socket, clientEncryptor: PKCS1OAEP_Cipher):
+    while True:
+        if s.fileno() == -1:
+            # socket closed
+            break
+        r, _, _ = select.select([s], [], [])
+        for rs in r:
+            if s == rs:
+                try:
+                    data = rs.recv(4096)
+                except OSError:
+                    # connection terminated (for some reason)
+                    break
+                if not data:
+                    break
+                msg = clientEncryptor.decrypt(data)
+                msg = pickle.loads(msg)
+                # TODO: HANDLE MESSAGE FORMATTING HERE
+                msgList.insert(tkinter.END, msg)
 
 
 def main():
@@ -86,25 +109,29 @@ def main():
         # not compare the two values. We trust the server to be the authority here, and to notify the client if the
         # handshake was performed incorrectly. In this case, the server notifies the client and terminates the
         # connection as normal.
-        sender = threading.Thread(target=send_handler, args=(s, serverEncryptor))
-        sender.start()
-        while True:
-            if s.fileno() == -1:
-                # socket closed
-                break
-            r, _, _ = select.select([s], [], [])
-            for rs in r:
-                if s == rs:
-                    try:
-                        data = rs.recv(4096)
-                    except OSError:
-                        # connection terminated (for some reason)
-                        break
-                    if not data:
-                        break
-                    msg = clientEncryptor.decrypt(data)
-                    msg = pickle.loads(msg)
-                    print(msg)
+
+        # GUI implementation is as follows
+        # TODO: Make it prettier
+        gui = tkinter.Tk()
+        gui.title(NICK)
+        chat_frame = tkinter.Frame(master=gui, width=100, height=200, bg="red")
+        scrollbar = tkinter.Scrollbar(chat_frame)
+        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        msglist = tkinter.Listbox(gui, bd=0, yscrollcommand=scrollbar.set, width=100)
+        msglist.pack(fill=tkinter.X)
+        chat_frame.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+        message_input = tkinter.Entry(gui, width=50)
+        message_button = tkinter.Button(gui, text="Send",
+                                        command=lambda: send_handler(s, serverEncryptor, message_input))
+        message_input.pack()
+        message_button.pack()
+        msglist.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=msglist.yview)
+
+        # launches server listener thread for incoming messages
+        receiver = threading.Thread(target=listener, args=(msglist, s, clientEncryptor,))
+        receiver.start()
+        tkinter.mainloop()
 
 
 if __name__ == "__main__":
